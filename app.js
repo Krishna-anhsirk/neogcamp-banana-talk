972be84b8fab06dd7d397ce16d39b455f4dc1af5
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-09-19 10:51:42 +0530

    input and output done with mock server

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputText = document.querySelector("#output");
+
+// var text = "Iron man";
+
+function urlConstructor(text) {
+  return (
+    "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text
+  );
+}
+
+function doTranslate(text) {
+  return fetch(urlConstructor(text))
+    .then((response) => response.json())
+    .then((data) => console.log(data.contents.translated, data.contents.text));
+}
 
 btnTranslate.addEventListener("click", () => {
-  console.log(txtInput.value);
+  doTranslate(txtInput.value);
 });
